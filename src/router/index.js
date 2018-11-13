6a02427599f42a79f33680acbe04c080111dfd38
Pongv <pongvarid@gmail.com>
2018-11-14 00:05:56 +0700

    Add Project

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+function loadView(view) {
+    return () => import(`../views/${view}.vue`)
+}
+
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: loadView('home')
+        },
+      
+    ]
+    
+})

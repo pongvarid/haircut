2dfa9c23a1c240bc07975a2fb009bd80e703c16d
Chitsnup <nup2540@gmail.com>
2018-12-15 00:38:32 +0700

    อัพเดทหน้าลงทะเบียน

--- src/views/bardetail.vue
+++ src/views/bardetail.vue
@@ -2,12 +2,11 @@
 <template>
     <div>
     <br><a @click="$router.push('/barregis')" class="icon icon-chevron-left"></a>
-     <label>ลงทะเบียนร้านตัดผม</label><br><br>
-    ชื่อร้าน :<at-input v-model="inputValue" placeholder="ชื่อร้าน"></at-input><br>
-    เบอร์โทร :<at-input v-model="inputValue" placeholder="เบอร์โทร" ></at-input><br>
-    ที่ตั้งร้าน :<at-input v-model="inputValue" placeholder="ที่ตั้งร้าน"></at-input><br>
-    <at-checkbox-group v-model="checkboxcloseday">
-        วันปิดทำการ : 
+     <label>ลงทะเบียนร้านตัดผม</label><br><br> 
+    ชื่อร้าน : <at-input v-model="inputValue" placeholder="ชื่อร้าน" style="width:100px"></at-input><br>
+    เบอร์โทร : <at-input v-model="inputValue" placeholder="เบอร์โทร" style="width:100px"></at-input><br>
+    ที่ตั้งร้าน : <at-textarea v-model="inputValue" placeholder="ที่ตั้งร้าน" style="width:200px"></at-textarea><br>
+    วันปิดทำการ : <at-checkbox-group v-model="checkboxcloseday">
         <at-checkbox label="จันทร์">จ.</at-checkbox>
         <at-checkbox label="อังการ">อ.</at-checkbox>
         <at-checkbox label="พุธ">พ.</at-checkbox>
@@ -22,11 +21,21 @@
         <at-option value="ทำสีผม" disabled>ทำสีผม</at-option>
         <at-option value="ดัดผม" disabled>ดัดผม</at-option>
         <at-option value="อบไอน้ำ" disabled>อบไอน้ำ</at-option>
-    </at-select><br><br>
-    
+    </at-select><br><br>  
+    เวลาทำต่อหัว :
+    <at-input-group v-model="inputprice">
+        <at-input v-model="inputValue" placeholder="ชาย" style="width:50px"></at-input> นาที
+        <at-input v-model="inputValue" placeholder="ราคา" style="width:50px"></at-input> บาท<br>
+        <at-input v-model="inputValue" placeholder="หญิง" style="width:50px"></at-input> นาที
+        <at-input v-model="inputValue" placeholder="ราคา" style="width:50px"></at-input> บาท<br>
+    </at-input-group>
+    <div class="row">
+        <at-button type="primary" icon="icon-plus"></at-button>
+    </div><br>
+
     <at-button class="box-brown shadow">สมัครสมาชิก</at-button><br><br>
     <at-button  @click="$router.push('/barlogin')" class="box shadow">ลงชื่อเข้าใช้</at-button><br>
-   
+    
     </div>
 </template>
  
